65ac223eccb421de253e8eba1fd6921fe1952ff0
Taiye Salami <taiye41@gmail.com>
2020-04-18 04:03:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,6 +10,7 @@ other_inner_hash = {}
 
 lib = YAML::load_file(File.join(__dir__, 'emoticons.yml'))
 
+<<<<<<< HEAD
   lib.each do |meaning, emo_arr|
     eng_emo = emo_arr[0]
     jap_emo = emo_arr[1]
@@ -23,12 +24,29 @@ lib = YAML::load_file(File.join(__dir__, 'emoticons.yml'))
   lib["get_emoticon"] = other_inner_hash
 
   lib
+=======
+lib.each do |meaning, emo_arr|
+  eng_emo = emo_arr[0]
+  jap_emo = emo_arr[1]
+
+  inner_hash[jap_emo] = meaning
+  other_inner_hash[eng_emo] = jap_emo
+end
+
+lib["get_meaning"] = inner_hash
+lib["get_emoticon"] = other_inner_hash
+
+lib.reject! { |k,v| k != get_meaning || k != get_emoticon }
+
+lib
+>>>>>>> c6ff2baf5f18cc9427d1104426c56511b8c9f80c
 
 end
 
 
 
 
+<<<<<<< HEAD
 def get_japanese_emoticon(emoticons, get_emoticon)
   # code goes here
   load_library(emoticons)["get_emoticon"].each do |k, v|
@@ -39,33 +57,22 @@ def get_japanese_emoticon(emoticons, get_emoticon)
 
   end
 "Sorry, that emoticon was not found"
+=======
+def get_japanese_emoticon
+  # code goes here
+
+>>>>>>> c6ff2baf5f18cc9427d1104426c56511b8c9f80c
 end
 
 def get_english_meaning(emoticons, get_meaning)
   # code goes here
+<<<<<<< HEAD
   load_library(emoticons)["get_meaning"].each do |k, v|
     if get_meaning == k
       return v
     end
   end
   "Sorry, that emoticon was not found"
+=======
+>>>>>>> c6ff2baf5f18cc9427d1104426c56511b8c9f80c
 end
-
-
-
-
-# inner_hash = {}
-#   lib.each do |meaning, emo_arr|
-#     emo_arr.each do |ele|
-#       eng_emo = ele[0]
-#       jap_emo = ele[1]
-#
-#       if !inner_hash[jap_emo]
-#         inner_hash[jap_emo] = meaning
-#       else inner_hash[jap_emo] = meaning
-#       end
-#
-#     end
-#   end
-#
-# inner_hash
